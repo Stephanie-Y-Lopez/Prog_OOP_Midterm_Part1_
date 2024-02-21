4b5a2e9dae09bd9e47b5e5137d64b959d53f91cf
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-02-21 13:24:56 -0800

    Testing

--- MyDatabase.cs
+++ MyDatabase.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 public static class MyDatabase
 {
-    private static List<string> data;
+    private static List<Product> data;
 
     static MyDatabase()
     {
         Console.WriteLine("The list is loading");
-        data = new List<string>
-        {
-            "Data 1",
-            "Data 2",
-            "Data 3",
-            "Data 4",
-            "Data 5"
-        };
-    }
+        Clothing tShirt = new Clothing("T-shirt", 19.99, "Medium", "Red");
+        Electronics smartphone = new Electronics("Smartphone", 799.99, "Apple");
+        Groceries banana = new Groceries("Banana", 0.99, "2024-02-10");
 
-    public static List<string> Data
-    {
-        get { return data; }
+        data = new List<Product>();
+
+        data.Add(tshirt);
+        data.Add(smartphone);
+        data.Add(banana);
     }
+        public static List<Product> Data
+        {
+            get { return data; }
+        }
 }
--- Program.cs
+++ Program.cs
@@ -19,18 +19,38 @@ namespace Prog_OOP_Midterm_Part1
 
             //Page 2: 
             // Creating a new instance of Groceries!
-            Clothing tShirt = new Clothing("T-shirt", 19.99, "Medium", "Red");
-            tShirt.DisplayProductInfo();
+            //Clothing Tshirt = new Clothing("T-shirt", 19.99, "Medium", "Red");
+            //Tshirt.DisplayProductInfo();
 
             // Testing Electronics
-            Electronics smartphone = new Electronics("Smartphone", 799.99, "Apple");
-            smartphone.DisplayProductInfo();
+            //Electronics Smartphone = new Electronics("Smartphone", 799.99, "Apple");
+            //Smartphone.DisplayProductInfo();
 
             // Testing Groceries
-            Groceries banana = new Groceries("Banana", 0.99, "2024-02-10");
-            banana.DisplayProductInfo();
+            //Groceries Banana = new Groceries("Banana", 0.99, "2024-02-10");
+            //Banana.DisplayProductInfo();
             //(I hope you dont mind I switched up the order of the items for the price to be shown 2nd instead of last because it makes more sense to me since it is also the second isle shown in the table.)
 
-        }
+            //Creating the list
+            List<Product> productList = new List<Product>();
+            productList.Add(Tshirt);
+            productList.Add(Smartphone);
+            productList.Add(Banana);
+
+            foreach (Product product in productList)
+            {
+
+                product.DisplayProductInfo();
+            }
+
+            // Access the static property to get the data
+            List<Product> dataList = MyDatabase.Data;
+
+            // Display the data from the static class
+            foreach (Product product in productList)
+            {
+
+                product.DisplayProductInfo();
+            }
     }
 }
